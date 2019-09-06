83491978acb14867dbdfc29ae82504e0ce244d09
JuyeonHong <coke1317@naver.com>
2019-09-07 00:03:50 +0900

    이미지 변경 및 추가

--- DragNDrop/ViewController.swift
+++ DragNDrop/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    fileprivate var items: [String] = ["0", "1", "2", "3", "4", "5", "6", "7"]
+    fileprivate var items: [String] = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
     
     @IBOutlet weak var collectionView: UICollectionView!
     
