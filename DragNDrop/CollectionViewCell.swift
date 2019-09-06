f0fda43c6aecd3b07844fc418dec2f5f923ee2ea
hongjuyeon <coke1317@naver.com>
2019-09-06 11:04:14 +0900

    first commit

---
+++ DragNDrop/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  DragNDrop
+//
+//  Created by hongjuyeon_dev on 29/08/2019.
+//  Copyright © 2019 hongjuyeon_dev. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
